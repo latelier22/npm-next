2f073ae1ea1ede69ced94a99ff4a961b6bc6c5fe
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-06-13 12:46:49 +0200

    HELLO button

--- app/components/MyButton.jsx
+++ app/components/MyButton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MyButton = ({children}) => {
   return (
-    <div className="bg-lime-400 p-4 m-4">
-      {children}
+    <div className="bg-lime-400 p-4 m-4 text-white border-solid bottom-2 border-white">
+      {children} HELLO
     </div>
   );
 };
