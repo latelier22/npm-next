f9cddee08724c0124935b0bf080f5cd2c8c2b02b
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-06-13 12:51:46 +0200

    build

--- dist/MyButton.js
+++ dist/MyButton.js
@@ -3,7 +3,7 @@ const MyButton = ({
   children
 }) => {
   return /*#__PURE__*/React.createElement("div", {
-    className: "bg-lime-400 p-4 m-4"
-  }, children);
+    className: "bg-lime-400 p-4 m-4 text-white border-solid bottom-2 border-white"
+  }, children, " HELLO");
 };
 export default MyButton;
