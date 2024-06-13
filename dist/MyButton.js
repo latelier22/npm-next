78ac39c576bfb03623ac3b79789096722720e899
Cyrille LE CORRE <latelier.cyrille@gmail.com>
2024-06-13 11:57:20 +0200

    babel build app/components

---
+++ dist/MyButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const MyButton = ({
+  children
+}) => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "bg-lime-400 p-4 m-4"
+  }, children);
+};
+export default MyButton;
